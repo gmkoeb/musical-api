61bf0c073905112bcb00249f07b18e48f1e0b1c8
gmkoeb <gmkoeb@gmail.com>
2025-09-01 10:58:58 -0300

    feat: create project using spring initialzr

---
+++ src/main/java/com/utfpr/musical_api/backend_musical_api/BackendMusicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.utfpr.musical_api.backend_musical_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMusicalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMusicalApiApplication.class, args);
+	}
+
+}
